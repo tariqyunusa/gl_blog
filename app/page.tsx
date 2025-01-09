7645836c5d67f616674bed40e31e96e7054d6683
Tariq Yunusa <tariikhyunusa@gmail.com>
2025-01-09 20:58:17 +0100

    added frtch on client side

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,34 @@
+"use client"
+import { useEffect, useState } from "react";
+import NavBar from "./adminComponents/NavBar";
+import axios from "axios";
 import Image from "next/image";
-import styles from "./page.module.css";
+
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  interface Blog {
+    Title: string;
+  }
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, debitis qui! Tenetur, labore veritatis quo obcaecati voluptate quos quidem illo odit dolore corporis in numquam beatae, porro cum, fuga reprehenderit.
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const [data, setData] = useState<Blog[]>([])
+  const fetchData =  async () => {
+    const response = await axios.get('/api/blog')
+    setData(response.data.blogs)
+    console.log(response.data.blogs)
+  }
+  useEffect(() => {
+    fetchData()
+  },[])
+
+  return (
+    <>
+    <NavBar />
+   <div>
+    <section>
+    
+    </section>
+   </div>
+    </>
   );
 }
