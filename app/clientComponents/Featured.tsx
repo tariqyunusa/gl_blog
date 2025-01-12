65e16ff3f403da91ed743dd2c397d66dff74261c
Tariq Yunusa <tariikhyunusa@gmail.com>
2025-01-12 12:17:17 +0100

    comlpleted featured section UI

--- app/clientComponents/Featured.tsx
+++ app/clientComponents/Featured.tsx
@@ -16,11 +16,16 @@ const Featured = ({data}: any): JSX.Element => {
   return (
     <section className={styles.featured__wrapper}>
       <Image fill src={data.image} alt={data.Title}/>
-      <h2>Featured</h2>
       <div className={styles.featured__content}>
+        <div className={styles.featured__header}>
+          <h2>Featured</h2>
+        </div>
+        <div className={styles.featured__text}>
         <h1>{data.Title}</h1>
         <p>{data.Description}</p>
         <button>Read More</button>
+        </div>
+        <div className={styles.featured__redundant}></div>
       </div>
     </section>
   )
