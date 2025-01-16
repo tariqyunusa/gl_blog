14b60a3e54ae3e8760879809cadb227ff4db8aff
Tariq Yunusa <tariikhyunusa@gmail.com>
2025-01-16 11:49:51 +0100

    fixed dropdown on different routes in NavBar

--- app/adminComponents/NavBar.tsx
+++ app/adminComponents/NavBar.tsx
@@ -1,19 +1,24 @@
+"use client"
 import React from 'react'
 import styles from '../styles/NavBar.module.css'
 import { Inspiration } from 'next/font/google'
 import DropDown from './DropDown'
+import { usePathname } from 'next/navigation'
 
 const inspiration = Inspiration({subsets: ["latin"], weight: "400"})
 
 
 const NavBar = () => {
+  const pathname = usePathname()
   return (
-    <nav className={styles.admin__navbar}>
-        <div className={styles.admin__navbar_filler} />
+    <nav className={styles.admin__navbar } style={{justifyContent: pathname.startsWith('/admin') ? 'space-between' : 'center'}}>
+        <div className={styles.admin__navbar_filler} style={{ display: pathname.startsWith('/admin') ? 'block' : 'none' }}/>
         <div className={styles.admin__navbar_logo}>
             <h1 className={`${inspiration.className} ${styles.NavBar__logo}`}>GL</h1>
         </div>
-        <DropDown />
+        {pathname.startsWith('/admin') && (
+          <DropDown />
+        )}
       
     </nav>
   )
